39977f32fe29a452048ee0f5edb50978765c1644
Твоя Булочка <85406297+Romb1k@users.noreply.github.com>
2021-06-08 01:12:32 +0300

    Add files via upload

---
+++ Counting DNA Nucleotides.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string str; 	cin >> str;
+	int A = 0, C = 0, G = 0, T = 0;
+	char sym;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		sym = str.at(i);
+		if (sym == 'A') A++;
+		if (sym == 'C') C++;
+		if (sym == 'G') G++;
+		if (sym == 'T') T++;
+	}
+
+
+	cout << A <<"\t" << C << "\t" << G << "\t" << T << "\n";
+	return 0;
+}
